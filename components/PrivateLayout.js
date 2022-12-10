eb3ec61432c455cd07de6b608541080a2cddbfac
Ngoc Thanh <ngocthanh@wearebloom.com.au>
2022-12-11 02:29:43 +0700

    FEAT: update authorize user

--- components/PrivateLayout.js
+++ components/PrivateLayout.js
@@ -1,14 +1,22 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
+import jwtDecode from "jwt-decode";
 
 const PrivateLayout = ({ children }) => {
-  const router = useRouter()
+  const router = useRouter();
 
   useEffect(() => {
     if (window) {
-      !localStorage.getItem('token') && router.push('/login')
+      const token = localStorage.getItem("token");
+      if (!token) {
+        router.push("/login");
+      } else {
+        if (jwtDecode(token).exp * 1000 < Date.now()) {
+          router.push("/login");
+        }
+      }
     }
-  }, [])
+  }, []);
 
   return children;
 };
