d63024bbbd90b176db6bc6fb832df5d0ef3d947c
Ngoc Thanh <ngocthanh@wearebloom.com.au>
2022-12-13 08:26:11 +0700

    FIX: update ui

--- pages/login.js
+++ pages/login.js
@@ -41,7 +41,7 @@ function Login() {
   return (
     <div
       style={{
-        background: '-webkit-radial-gradient( 0% 100%, ellipse cover, rgba(104, 128, 138, 0.4) 10%, rgba(138, 114, 76, 0) 40% ), linear-gradient( to bottom, rgba(57, 173, 219, 0.25) 0%, rgba(42, 60, 87, 0.4) 100% ), linear-gradient(135deg, #670d10 0%, #092756 100%);',
+        background: 'linear-gradient(135deg, #670d10 0%, #092756 100%)',
       }}
       className={
         'min-h-screen flex flex-col items-center justify-center text-white text-center'
@@ -55,14 +55,12 @@ function Login() {
             initialValues={{ remember: true }}
             onFinish={onFinish}
             autoComplete="off"
-            className='text-left flex flex-col gap-2'
+            className="text-left flex flex-col gap-2"
           >
             <label className="text-white">Tên đăng nhập:</label>
             <Form.Item
               name="username"
-              rules={[
-                { required: true, message: 'Nhập tên đăng nhập!' },
-              ]}
+              rules={[{ required: true, message: 'Nhập tên đăng nhập!' }]}
               className="sm:mb-2"
             >
               <Input />
@@ -70,9 +68,7 @@ function Login() {
             <label className="text-white">Mật khẩu:</label>
             <Form.Item
               name="password"
-              rules={[
-                { required: true, message: 'Nhập mật khẩu!' },
-              ]}
+              rules={[{ required: true, message: 'Nhập mật khẩu!' }]}
               className="sm:mb-2"
             >
               <Input.Password />
