4d616225dc1c6eb35531b908e83d24b61c39bd7d
Ngoc Thanh <ngocthanh@wearebloom.com.au>
2022-12-14 21:06:45 +0700

    FIX: update locale datepicker

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,23 @@
-import { ApolloProvider } from "@apollo/client";
-import client from "../apollo-client";
-import Head from "next/head";
-import "antd/dist/reset.css";
-import "../styles/vars.css";
-import "../styles/global.css";
+import { ApolloProvider } from '@apollo/client';
+import client from '../apollo-client';
+import Head from 'next/head';
+import 'antd/dist/reset.css';
+import '../styles/vars.css';
+import '../styles/global.css';
+import { ConfigProvider } from 'antd';
+import 'dayjs/locale/vi';
+import locale from 'antd/locale/vi_VN';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={client}>
-      <Head>
-        <title>Credit App</title>
-        <link href="/dist/output.css" rel="stylesheet" />
-      </Head>
-      <Component {...pageProps} />
+      <ConfigProvider locale={locale}>
+        <Head>
+          <title>Credit App</title>
+          <link href="/dist/output.css" rel="stylesheet" />
+        </Head>
+        <Component {...pageProps} />
+      </ConfigProvider>
     </ApolloProvider>
   );
 }
