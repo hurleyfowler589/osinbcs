dbbaddd1f5c1a883aeead9884899f8e828bdb1a8
hurleyfowler589 <hurleyfowler589@gmail.com>
2023-03-07 07:00:11 -0800

    pp

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 const Error404Page = () => (
   <div className="text-red-500 min-h-screen flex items-center justify-center mb-4">
   <div className="container mx-auto px-4 flex flex-col flex-1 items-center justify-center text-center">
-    <h1 className="text-6xl xl:text-9xl font-bold ">4044444444</h1>
+    <h1 className="text-6xl xl:text-9xl font-bold ">404</h1>
     <h2 className="text-4xl xl:text-6xl font-semibold mt-4">
       Page not found
     </h2>
