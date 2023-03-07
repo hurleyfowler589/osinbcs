6c230992a797b5dbe8774b96c4d6783b05e14a53
hurleyfowler589 <hurleyfowler589@gmail.com>
2023-03-07 06:48:41 -0800

    pp

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  watch: true,
   env: {
     GRAPPLE_API_URI: process.env.GRAPPLE_API_URI,
     GRAPPLE_API_GRAPHQL_PATH: process.env.GRAPPLE_API_GRAPHQL_PATH,
